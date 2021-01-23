07f1431cacb70279191283746b1b81f81dda6bea
zcammack <zack.cammack@gmail.com>
2021-01-23 16:11:43 -0600

    Added some more styling edits. Changed out some settings in application.css to accomadate new styling.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+  devise :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :validatable,
+    :omniauthable, omniauth_providers: %i[facebook]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
