9b1499401bc8646b4dc465ebcfe90bc3efcce339
zcammack <zack.cammack@gmail.com>
2021-01-23 17:39:55 -0600

    Added Room resource.

---
+++ db/migrate/20210123233934_create_reservations.rb
@@ -0,0 +1,14 @@
+class CreateReservations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reservations do |t|
+      t.string :duration
+      t.string :party_size
+      t.date :start_date
+      t.date :end_date
+      t.references :room, null: false, foreign_key: true
+      t.references :tenant, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
