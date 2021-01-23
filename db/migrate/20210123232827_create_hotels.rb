6dc9d3a152df5e8a23da03b4cddc810da6e49daa
zcammack <zack.cammack@gmail.com>
2021-01-23 17:38:16 -0600

    Creating resources using the resource generator.

---
+++ db/migrate/20210123232827_create_hotels.rb
@@ -0,0 +1,13 @@
+class CreateHotels < ActiveRecord::Migration[6.0]
+  def change
+    create_table :hotels do |t|
+      t.string :size
+      t.string :vacancies
+      t.boolean :wifi
+      t.boolean :breakfast
+      t.string :accessibility
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210123233753_create_rooms.rb
@@ -0,0 +1,14 @@
+class CreateRooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :rooms do |t|
+      t.string :occupancy
+      t.boolean :vacant
+      t.string :floor_number
+      t.boolean :smoking
+      t.string :price_per_night
+      t.references :hotel, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
