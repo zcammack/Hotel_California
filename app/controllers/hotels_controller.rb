eebd9b3e5a1f4a669f8cd8c20252ea9dc5107d36
zcammack <zack.cammack@gmail.com>
2021-01-25 10:59:33 -0600

    Working on some helper methods to use across the application.

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,3 @@
 class HotelsController < ApplicationController
+    before_action :redirect_if_not_logged_in
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,3 @@
 class ReservationsController < ApplicationController
+    before_action :redirect_if_not_logged_in
 end
