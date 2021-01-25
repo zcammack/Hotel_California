2ab2193554329ac0d3ab8dca8a5110fd39d11fd2
zcammack <zack.cammack@gmail.com>
2021-01-25 11:42:20 -0600

    Worked on creating more pages and building basic layout.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,3 +1,11 @@
 class ReservationsController < ApplicationController
     before_action :redirect_if_not_logged_in
+
+    def new
+        @reservation = Reservation.new
+    end
+
+    def create
+        binding.pry
+    end
 end
