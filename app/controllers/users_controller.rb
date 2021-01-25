2a880753795274c66cab96e5b15bad9a546909be
zcammack <zack.cammack@gmail.com>
2021-01-25 10:22:48 -0600

    Added sessions controller. Added onto users show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,23 @@ class UsersController < ApplicationController
     end
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def show
-
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user
     end
 
     private
 
-    
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
 
+  root 'sessions#home'
+
   get 'signup' => 'users#new'
   post 'signup' => 'users#create'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
 
   resources :tenants
   resources :reservations
   resources :rooms
   resources :users
   resources :hotels
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
