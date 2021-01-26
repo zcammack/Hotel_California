7271d83e195e77200a56e5d2e05bc519ef44292e
zcammack <zack.cammack@gmail.com>
2021-01-26 10:35:43 -0600

    Replaced user model with Devise user model.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'pry'
 
+gem 'devise'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'sessions#home'
-
-  get 'signup' => 'users#new'
-  post 'signup' => 'users#create'
-
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-
-  delete 'logout' => 'sessions#destroy'
 
   resources :tenants do
     resources :reservations
   end
 
-  resources :users
   resources :hotels do
     resources :rooms
   end
