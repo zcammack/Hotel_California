4c1fcb3cf21fea22e7c0431dc162858fdd53e043
zcammack <zack.cammack@gmail.com>
2021-01-26 08:51:42 -0600

    Created full CRUD functionality for hotel controller. Going to change it so users have many hotels.

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,5 +1,5 @@
 class HotelsController < ApplicationController
-    before_action :redirect_if_not_logged_in
+    before_action :redirect_if_not_logged_in, :set_hotel, only: [:show, :edit, :update, :destroy]
 
     def index
         @hotels = Hotel.all
@@ -7,5 +7,46 @@ class HotelsController < ApplicationController
 
     def show
        @hotel = Hotel.find_by_id(params[:id])
+    end
+
+    def new
+        @hotel = Hotel.new
+    end
+
+    def edit
+
+    end
+
+    def create
+        @hotel = Hotel.new(hotel_params)
+        if @hotel.save
+            redirect_to @hotel, notice: 'Hotel was successfully created.'
+        else
+            render :new, notice: 'Hotel was not created.'
+        end
+    end
+
+    def update
+        if @hotel.update(hotel_params)
+            redirect_to @hotel, notice: 'Hotel was successfully updated.'
+        else
+            render :edit, notice: 'Hotel has not been changed.'
+        end
+    end
+
+    def destroy
+        @hotel.destroy
+        redirect_to hotels_url, notice: 'Hotel was successfully deleted.'
+    end
+
+
+    private
+
+    def set_hotel
+        @hotel = Hotel.find(params[:id])
+    end
+
+    def hotel_params
+        params.require(:hotel).permit(:name, :size, :wifi, :breakfast, :accessibility)
     end
 end
